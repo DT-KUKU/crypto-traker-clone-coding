bdbc60c8beb6a0b66c805db40078be1c037e6238
kuku <covernat6949@gmail.com>
2023-02-22 16:34:12 +0900

    [Feat] 가격 부분 추가

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -7,10 +7,26 @@ const PriceContainer = styled.div`
   gap: 20px;
 `;
 
-const PriceBox = styled.div`
+const PriceBox = styled.div<{ isBoolean: boolean }>`
   border: 1px solid teal;
   background-color: white;
   border-radius: 7px;
+  height: 150px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > h1 {
+    flex: 1;
+    font-size: 25px;
+  }
+  > p {
+    flex: 2;
+    width: 100%;
+    text-align: center;
+    line-height: 50px;
+    font-size: 35px;
+    color: ${(props) => (props.isBoolean ? "red" : "blue")};
+  }
 `;
 
 interface IPricePropsType {
@@ -18,25 +34,24 @@ interface IPricePropsType {
 }
 
 function Price({ price }: IPricePropsType) {
+  console.log(typeof price.quotes.USD.percent_change_1h);
   return (
     <PriceContainer>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
+      <PriceBox isBoolean={price.quotes.USD.percent_change_1h > 0}>
+        <h1>1h</h1>
+        <p>{price.quotes.USD.percent_change_1h}</p>
       </PriceBox>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
+      <PriceBox isBoolean={price.quotes.USD.percent_change_6h > 0}>
+        <h1>6h</h1>
+        <p>{price.quotes.USD.percent_change_6h}</p>
       </PriceBox>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
+      <PriceBox isBoolean={price.quotes.USD.percent_change_12h > 0}>
+        <h1>12h</h1>
+        <p>{price.quotes.USD.percent_change_12h}</p>
       </PriceBox>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
-      </PriceBox>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
-      </PriceBox>
-      <PriceBox>
-        <h1>{price.id} 1h</h1>
+      <PriceBox isBoolean={price.quotes.USD.percent_change_24h > 0}>
+        <h1>24h</h1>
+        <p>{price.quotes.USD.percent_change_24h}</p>
       </PriceBox>
     </PriceContainer>
   );
